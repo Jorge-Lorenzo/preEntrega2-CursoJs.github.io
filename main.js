c3db9f5f41888b38d56f4bdc273f1c5ce8bf6ff1
Jorge Lorenzo <jglorenzo56@gmail.com>
2022-12-20 23:00:19 +0100

    commit

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 let edad= 18;
 let ingresEdad= parseInt(prompt("Por favor ingrese su edad"));
-let formatoTexto= String;
 
 if (ingresEdad >= edad) {
     alert("Bienvenido a nuestro sitio");    
@@ -10,7 +9,7 @@ else if (ingresEdad < edad) {
     
 }
 
-//total compra carrito
+//TOTAL COMPRA DE CARRITO
 const carrito= [
     {nombre: "Vino Catena Zapata Malbec", precio: 3250 },
     {nombre: "Vino Mosquita Muerta Tinto", precio: 3100},
@@ -22,24 +21,26 @@ let total = carrito.reduce((accum, p) => {
     return accum + p.precio;
 },0);
 
-console.log("El total de su compra es de " + total + " pesos." );
+alert("El total de su compra es de " + total + " pesos.");
 
 function primero(segundo) {
-    setTimeout(function() {
-        console.log("RECIBO:  ","Vino Catena Zapata Malbec, precio: 3250  ",
-                    "Vino Mosquita Muerta Tinto, precio: 3100  ",
-                    "Vino Alamos Malbec precio: 900  ",
-                    "gin tanqueray, precio:5400");
-                    segundo()
-        },3000)
-    
+        alert("RECIBO:  Vino Catena Zapata Malbec precio: 3250, Vino Mosquita Muerta Tinto precio: 3100 Vino Alamos Malbec precio: 900, gin tanqueray, precio:5400".toUpperCase());
+        segundo()
 }
 
 function segundo(){
-    console.log("GRACIAS POR ELEGIRNOS");
+    alert("GRACIAS POR ELEGIRNOS");
 }
 
 primero(segundo);
 
-    
+//TOTAl DE COMPRA CON PRECIO DE ENVÍO
 
+const flete = carrito.map((el) => {
+    return {
+        nombre: el.nombre,
+        precio: el.precio + 1000
+    }
+})
+
+alert(precio)
